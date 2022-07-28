0f8eb00deee2b8c526023f5f7b66fac1a75a16df
Lizard <lizardwine@hotmail.com>
2022-07-28 17:00:45 -0500

    deleted termios

--- misc.hpp
+++ misc.hpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <termios.h>
 #include <stdio.h>
 #include <sqlite3.h>
 #include <iostream>  
@@ -135,52 +134,7 @@ bool download(const std::string& url, const std::string& file_path)
         return false;
     }
 }
-static struct termios old, current;
 
-/* Initialize new terminal i/o settings */
-void initTermios(int echo)
-{
-    tcgetattr(0, &old);         /* grab old terminal i/o settings */
-    current = old;              /* make new settings same as old settings */
-    current.c_lflag &= ~ICANON; /* disable buffered i/o */
-    if (echo)
-    {
-        current.c_lflag |= ECHO; /* set echo mode */
-    }
-    else
-    {
-        current.c_lflag &= ~ECHO; /* set no echo mode */
-    }
-    tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
-}
-
-/* Restore old terminal i/o settings */
-void resetTermios(void)
-{
-    tcsetattr(0, TCSANOW, &old);
-}
-
-/* Read 1 character - echo defines echo mode */
-char getch_(int echo)
-{
-    char ch;
-    initTermios(echo);
-    ch = getchar();
-    resetTermios();
-    return ch;
-}
-
-/* Read 1 character without echo */
-char getch(void)
-{
-    return getch_(0);
-}
-
-/* Read 1 character with echo */
-char getche(void)
-{
-    return getch_(1);
-}
 
 string read_file(string file){
     string lines = "";
